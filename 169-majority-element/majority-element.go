1d7e2f9a15d9b594075185a4cb6ebaf26de1b590
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-09-04 17:46:36 +0530

    Time: 19 ms (64.01%) | Memory: 6.4 MB (92.19%) - LeetSync

--- 169-majority-element/majority-element.go
+++ 169-majority-element/majority-element.go
@@ -1,16 +1,20 @@
 func majorityElement(nums []int) int {
     n := len(nums)
-    counts := make(map[int]int,n)
+    if n == 1 {return nums[0]}
+    
+    // moore's best voting algorithm
+    majorityElem := nums[0]
+    count := 1
+    
+    for i:=1;i<n;i++ {
+        if count == 0 {majorityElem = nums[i]}
+        if nums[i] == majorityElem {
+            count++
+        } else {count--}
 
-    for _,v := range nums {
-        counts[v]++
+        
     }
+    
+    return majorityElem
 
-    for key,value := range counts {
-        if value > n/2 { 
-            return key
-        }
-    }
-
-    return 0
 }
