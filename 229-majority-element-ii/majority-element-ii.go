353b056d3c7ad0cc1b83d1a232a54fd58b863bda
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-09-05 08:40:56 +0530

    Time: 6 ms (85.78%) | Memory: 5.7 MB (45.59%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.go
+++ 229-majority-element-ii/majority-element-ii.go
@@ -1,19 +1,41 @@
 func majorityElement(nums []int) []int {
-    
-    n := len(nums)
-    counts := make(map[int]int,n)
-    result := make([]int,0)
+    count1, count2, num1, num2 := 0, 0, 0, 0
 
-    for _,value := range nums {
-        counts[value]++
-    }
-
-    for key,value := range counts {
-        if value > n/3 {
-            result = append(result,key)
+    // We can have only 2 numbers with count greater than len(nums)/3
+    // Find those 2 possible numbers first
+    for _, num := range nums {
+        if num == num1 {
+            count1++
+        } else if num == num2 {
+            count2++
+        } else if count1 == 0 {
+            num1, count1 = num, 1
+        } else if count2 == 0 {
+            num2, count2 = num, 1
+        } else {
+            count1--
+            count2--
         }
     }
 
-    return result
+    // Verify count of those 2 possible numbers again
+    count1, count2 = 0, 0
+    for _, num := range nums {
+        if num == num1 {
+            count1++
+        } else if num == num2 {
+            count2++
+        }
+    }
 
+    // Add those numbers if they satisfy conditions
+    res := []int{}
+    if count1 > len(nums) / 3 {
+        res = append(res, num1)
+    }
+    if count2 > len(nums) / 3 {
+        res = append(res, num2)
+    }
+
+    return res
 }
