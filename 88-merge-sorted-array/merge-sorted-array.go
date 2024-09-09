b912c042ad666a8e8cb6a988bad8d8bb26e3052c
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-09-09 14:33:42 +0530

    Time: 0 ms (100.00%) | Memory: 2.5 MB (9.17%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.go
@@ -0,0 +1,34 @@
+func merge(nums1 []int, m int, nums2 []int, n int)  []int{
+     
+	i , j := 0,0
+	for j<n && i < len(nums1){
+	   if nums1[i] == nums2[j] {
+		   insert(nums1,i,nums2[j])
+		   i++;j++
+	   } else if nums1[i] > nums2[j] {
+		   insert(nums1,i,nums2[j])
+		   j++
+	   } else {i++}
+	}
+
+	if j < n {
+	   b := len(nums1) - 1
+	   for k := n-1;k>=j;k-- {
+		   nums1[b] = nums2[k]
+		   b--
+	   }
+	}
+
+	return nums1
+
+}
+
+func insert(arr []int,idx,value int) {
+ 
+   
+   for i := len(arr)-2;i>=idx;i-- {
+	   arr[i+1] = arr[i]
+   }
+
+   arr[idx] = value
+}
