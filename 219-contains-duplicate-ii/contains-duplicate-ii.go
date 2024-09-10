445a7b33555a0d0c0ce902dac03bf3599ab4cfd9
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-09-10 21:20:00 +0530

    Time: 107 ms (33.52%) | Memory: 16.7 MB (13.21%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.go
@@ -0,0 +1,16 @@
+func containsNearbyDuplicate(nums []int, k int) bool {
+
+	n := len(nums)
+    maps := make(map[int]int,n)
+
+    for idx,val := range nums {
+        if v,ok := maps[val];ok {
+            if math.Abs(float64(v-idx)) <= float64(k) {
+                return true
+            }
+        }
+        maps[val] = idx
+    }
+	
+	return false
+}
