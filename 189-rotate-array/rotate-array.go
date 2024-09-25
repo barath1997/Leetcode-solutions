dce954517ee04e41063a1520767c4bbfadce255c
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-09-25 19:41:44 +0530

    Time: 27 ms (46.56%) | Memory: 7.7 MB (52.74%) - LeetSync

--- 189-rotate-array/rotate-array.go
+++ 189-rotate-array/rotate-array.go
@@ -1,20 +1,37 @@
-func rotate(nums []int, k int)  {
+// optimised solution
+// func rotate(nums []int, k int)  {
 
-
-    n := len(nums)
-    k = k%n
-    begin,end := 0,n-1
+//     n := len(nums)
+//     k = k%n
+//     begin,end := 0,n-1
     
-    reverse(nums,begin,end)
-    reverse(nums,begin,k-1)
-    reverse(nums,k,end)
-}
+//     reverse(nums,begin,end)
+//     reverse(nums,begin,k-1)
+//     reverse(nums,k,end)
+// }
 
-func reverse(arr []int,begin,end int) {
-    for begin <= end {
-        temp := arr[begin]
-        arr[begin] = arr[end]
-        arr[end] = temp
-        begin++;end--
+// func reverse(arr []int,begin,end int) {
+//     for begin <= end {
+//         temp := arr[begin]
+//         arr[begin] = arr[end]
+//         arr[end] = temp
+//         begin++;end--
+//     }
+// }
+
+// solution with O(n) extra space
+
+func rotate(nums []int,k int) {
+    
+    n := len(nums)
+    newArr := make([]int,n)
+
+    for idx,val := range nums {
+        newArr[idx] = val
     }
+
+    for i:=0;i<n;i++ {
+        nums[(i+k)%n] = newArr[i]
+    }
+
 }
