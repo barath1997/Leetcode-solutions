8c20bd8cd4639b3b44afd24d03352f91edfaf665
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-09-29 10:01:57 +0530

    Time: 17 ms (24.39%) | Memory: 6 MB (91.13%) - LeetSync

--- 268-missing-number/missing-number.go
+++ 268-missing-number/missing-number.go
@@ -10,12 +10,22 @@ func missingNumber(nums []int) int {
     return noOfIterations*/
 
     // optimised
-    sum := 0
+    /*sum := 0
     for _,v := range nums {
         sum+=v
     }
     n := len(nums)
-    //sum of numbers form 0...n
+    //sum of numbers from 0...n
     expectedSum := ((n+1)*n)/2
-    return expectedSum - sum
+    return expectedSum - sum*/
+
+    // using bit maniluation
+    sort.Ints(nums)
+    noOfIterations := nums[len(nums)-1] - nums[0] + 1
+    for i:=0;i<noOfIterations;i++ {
+        if i ^ nums[i] != 0 {
+            return i
+        }
+    }
+    return noOfIterations
 }
