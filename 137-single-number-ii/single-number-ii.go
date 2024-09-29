9fd61a44eeafaa43bf061dd5279466979d3738c6
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-09-29 21:22:47 +0530

    Time: 4 ms (70.44%) | Memory: 3.4 MB (99.64%) - LeetSync

--- 137-single-number-ii/single-number-ii.go
+++ 137-single-number-ii/single-number-ii.go
@@ -1,6 +1,6 @@
 func singleNumber(nums []int) int {
    
-    n := len(nums) 
+    /*n := len(nums) 
     sort.Ints(nums)
     for i:=1;i<n;i+=3 {
         if nums[i] == nums[i-1] && nums[i] == nums[i+1] {
@@ -11,5 +11,14 @@ func singleNumber(nums []int) int {
 
     }
 
-    return nums[n-1]
+    return nums[n-1]*/
+
+    //optimised solution : link : https://www.youtube.com/watch?v=5Bb2nqA40JY
+    bucket1,bucket2 := 0,0
+    for _,v := range nums {
+       bucket1 = (bucket1 ^ v) & ^bucket2
+       bucket2 = (bucket2 ^ v) & ^bucket1
+    }
+
+    return bucket1
 }
