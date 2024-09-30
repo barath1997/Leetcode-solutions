9366b849af4f290486b0fca74754a5501f41d46e
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-09-30 09:27:12 +0530

    Time: 5 ms (72.86%) | Memory: 3.6 MB (100.00%) - LeetSync

--- 260-single-number-iii/single-number-iii.go
+++ 260-single-number-iii/single-number-iii.go
@@ -1,19 +1,39 @@
 func singleNumber(nums []int) []int {
 
-    // map based soln : TC : O(n) , SC : O(n)
-    counts := make(map[int]int,len(nums))
-    result := make([]int,2)
-    for _,v := range nums {
-        counts[v]++
-    }
-    
-    idx := 0
-    for val,count := range counts {
-        if count == 1 {
-          result[idx] = val
-          idx++
-        }
-    }
+	// map based soln : TC : O(n) , SC : O(n)
+	/*counts := make(map[int]int,len(nums))
+	  result := make([]int,2)
+	  for _,v := range nums {
+	      counts[v]++
+	  }
 
-    return result
+	  idx := 0
+	  for val,count := range counts {
+	      if count == 1 {
+	        result[idx] = val
+	        idx++
+	      }
+	  }
+
+	  return result*/
+      
+      // optimised solution
+      xoredValue := 0 
+
+      for _,v := range nums {
+        xoredValue^=v
+      }
+
+      val := (xoredValue & (xoredValue-1)) ^ xoredValue
+
+      bucket1,bucket2 := 0,0
+      for _,v := range nums {
+        if v & val == 0  {
+            bucket1 ^= v
+        } else {
+            bucket2 ^= v
+        }
+      }
+
+      return []int{bucket1,bucket2}
 }
