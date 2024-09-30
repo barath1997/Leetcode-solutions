59c415046da56949998261c34a4b95354a393c27
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-09-30 09:29:44 +0530

    Time: 2 ms (91.43%) | Memory: 3.6 MB (94.29%) - LeetSync

--- 260-single-number-iii/single-number-iii.go
+++ 260-single-number-iii/single-number-iii.go
@@ -1,21 +1,4 @@
 func singleNumber(nums []int) []int {
-
-	// map based soln : TC : O(n) , SC : O(n)
-	/*counts := make(map[int]int,len(nums))
-	  result := make([]int,2)
-	  for _,v := range nums {
-	      counts[v]++
-	  }
-
-	  idx := 0
-	  for val,count := range counts {
-	      if count == 1 {
-	        result[idx] = val
-	        idx++
-	      }
-	  }
-
-	  return result*/
       
       // optimised solution
       xoredValue := 0 
