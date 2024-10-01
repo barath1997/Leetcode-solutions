537db24a0cbff3bd0aff2216dd661736de9435fa
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-10-01 21:33:06 +0530

    Time: 0 ms (100.00%) | Memory: 2.2 MB (75.00%) - LeetSync

--- 810-valid-tic-tac-toe-state/valid-tic-tac-toe-state.go
+++ 810-valid-tic-tac-toe-state/valid-tic-tac-toe-state.go
@@ -1,3 +1,13 @@
+/*Types of invalid states:
+
+Number of O > Number of X
+Number of X - Number of O > 1
+greater than 2 Win states for X or O
+Both X and O have a win state
+X wins but Number of X == Number of O
+O wins but Number of O < Number of X
+*/
+
 func validTicTacToe(board []string) bool {
 
 	full := ""
@@ -13,10 +23,12 @@ func validTicTacToe(board []string) bool {
 		}
 	}
 
+    // if all are spaces  ["   ","   ","   "]
 	if spaces == 9 {
 		return true
 	}
-
+    
+    // move row by row and then compare elements in each row
 	for i := 0; i < 9; i += 3 {
 		if (full[i] == full[i+1]) && (full[i+1] == full[i+2]) {
 			if full[i] == 'X' {
@@ -26,7 +38,8 @@ func validTicTacToe(board []string) bool {
 			}
 		}
 	}
-
+    
+    // move column by column and then compare elements in each column
 	for j := 0; j < 3; j++ {
 		if (full[j] == full[j+3]) && (full[j+3] == full[j+6]) {
 			if full[j] == 'X' {
@@ -36,7 +49,8 @@ func validTicTacToe(board []string) bool {
 			}
 		}
 	}
-
+    
+    // compare elements in leading diagonal
 	if (full[0] == full[4]) && (full[4] == full[8]) {
 		if full[0] == 'X' {
 			xWins++
@@ -44,7 +58,8 @@ func validTicTacToe(board []string) bool {
 			oWins++
 		}
 	}
-
+    
+    // comapre elements in the secondary diagonal
 	if (full[2] == full[4]) && (full[4] == full[6]) {
 		if full[2] == 'X' {
 			xWins++
@@ -64,25 +79,36 @@ func validTicTacToe(board []string) bool {
 		}
 	}
 
+    // validating conditions inputs like ["O  ","   ","   "] :starting with O or generally always X >= O because we start with X.  
 	if oCount > xCount {
 		return false
 	}
+
+    // if the player puts X consecutively without giving chance to O
 	if xCount-oCount > 1 {
 		return false
 	}
-
+    
+    // we can have at max 2 win states for both X and O in a game
 	if xWins > 2 || oWins > 2 {
 		return false
 	}
+
+    // both X and O cant win in one game, there can be only one winner, then game stops
 	if xWins > 0 && oWins > 0 {
 		return false
 	}
+
+    // if X won then his X count should be more than O because we start with X if both are equal then there should not be a win state.
 	if xWins > 0 && xCount == oCount {
 		return false
 	}
+
+    // if O wins , then O count should be at max equal to X count, if O wins then game finishes then O count becomes equal to X count , because we start with X.
 	if oWins > 0 && oCount < xCount {
 		return false
 	}
-
+    
+    // if all conditions fails , return true
 	return true
 }
