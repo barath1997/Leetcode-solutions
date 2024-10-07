2aaa4a5b9d4b7d157209f95ce95e03cb11a36a4d
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-10-07 21:40:58 +0530

    Time: 3 ms (42.46%) | Memory: 2.2 MB (34.20%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.go
+++ 191-number-of-1-bits/number-of-1-bits.go
@@ -1,11 +1,11 @@
 func hammingWeight(n int) int {
     
     count := 0
-    for n != 0 {
+    for n != 0 { 
     if n & 1 == 1 { 
         count++ 
     }
-     n >>= 1
+     n >>= 1   // right shift 10>>1 is 5 and 5>>1 is 2 and 2>>1 = 1 , n keeps diving 2 on right shift and        the setbits keep decreasing on every rightshift, if all setbits become 0 then n becomes 0 
     }
 
     return count
