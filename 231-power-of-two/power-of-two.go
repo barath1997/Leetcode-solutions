00282256fe69add3771407ad739bbfdcaf2e3893
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-10-08 19:02:28 +0530

    Time: 0 ms (100.00%) | Memory: 2.4 MB (11.83%) - LeetSync

--- 231-power-of-two/power-of-two.go
+++ 231-power-of-two/power-of-two.go
@@ -1,15 +1,4 @@
 func isPowerOfTwo(n int) bool {
     
-   if n < 1 {return false}
-   if n == 1 { return true }
-
-   if n % 2 == 0 {
-      
-    if n & (n-1) == 0 {return true}
-
-   }
-
-   return false
-
-
+  return n > 0 && (n & (n-1)) == 0
 }
