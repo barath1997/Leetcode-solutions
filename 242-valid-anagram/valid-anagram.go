401925de7493b159d512b17b0b341d93cc89a34e
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-10-19 10:46:07 +0530

    Time: 5 ms (68.61%) | Memory: 4.5 MB (7.32%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.go
@@ -0,0 +1,25 @@
+func isAnagram(s string, t string) bool {
+
+	stMap := make(map[rune]int, 0)
+
+	for _, v := range s {
+		stMap[v]++
+	}
+
+	for _, v := range t {
+		if val := stMap[v]; val > 0 {
+			stMap[v]--
+		} else {
+			return false
+		}
+	}
+
+    for _,v := range stMap {
+        if v > 0 {
+            return false
+        }
+    }
+
+	return true
+
+}
