cfd5acc0ae9932ed55d80fd7f3d9f8ae3a42a811
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-11-09 09:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (5.02%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.go
@@ -0,0 +1,55 @@
+func minAddToMakeValid(s string) int {
+    
+	openCount,closeCount := 0,0
+	var stack Stack
+ 
+	for _,v := range s {
+	   val := string(v)
+	   if val == "(" {
+		 stack.Push(val)
+		 closeCount++
+	   } else {
+		 bracket,_ := stack.Peek()
+		 if bracket == "(" {
+			 stack.Pop()
+			 closeCount--
+		 } else {
+			 openCount ++
+			 stack.Push(val)
+		 }
+	   }
+ 
+	}
+	return openCount + closeCount
+ 
+ }
+ 
+ type Stack []string
+ 
+ func (s *Stack) IsEmpty() bool {
+	 return len(*s) == 0
+ }
+ 
+ // Push a new value onto the stack
+ func (s *Stack) Push(num string) {
+	 *s = append(*s, num) // Simply append the new value to the end of the stack
+ }
+ 
+ // Remove and return top element of stack. Return false if stack is empty.
+ func (s *Stack) Pop() (string, bool) {
+	 if s.IsEmpty() {
+		 return "", false
+	 } else {
+		 index := len(*s) - 1   // Get the index of the top most element.
+		 element := (*s)[index] // Index into the slice and obtain the element.
+		 *s = (*s)[:index]      // Remove it from the stack by slicing it off.
+		 return element, true
+	 }
+ }
+ 
+ func (s *Stack) Peek() (string, bool) {
+	 if !s.IsEmpty() {
+		 return (*s)[len(*s)-1], true
+	 }
+	 return "", false
+ }
