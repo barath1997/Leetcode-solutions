0f02d26b3ed05010282c9909102cae80fa01c4d5
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-11-12 21:22:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (51.61%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.go
+++ 503-next-greater-element-ii/next-greater-element-ii.go
@@ -1,3 +1,54 @@
+// STACK SOLUTION DISCUSSED IN CLASS
+func nextGreaterElements(nums []int) []int {
+   
+   var stack Stack1
+   n := len(nums)
+   result := make([]int,n)
+
+   for idx,_ := range nums {
+      result[idx] = -1
+   }
+
+   for i:=0;i<2*n;i++ {
+       
+       for !stack.IsEmpty() && nums[stack.Peek()] < nums[i%n] {
+         result[stack.Peek()] = nums[i%n]
+         stack.Pop()
+       }
+       
+       if i<n {
+        stack.Push(i)
+       }
+   }
+
+   return result
+}
+
+type Stack1 []int
+
+func (s *Stack1) IsEmpty() bool{
+    return len(*s) == 0
+}
+
+func (s *Stack1) Push(val int) {
+	*s = append(*s, val)
+}
+
+func (s *Stack1) Peek() int {
+	if len(*s) != 0 {
+		dd := *s
+		return dd[len(*s)-1]
+	}
+	return math.MinInt
+}
+
+func (s *Stack1) Pop() {
+	if len(*s) != 0 {
+		dd := *s
+		*s = dd[:len(*s)-1]
+	}
+}
+
 // MONOTONIC STACK
 // func nextGreaterElements(nums []int) []int {
 // 	initialLength := len(nums)
@@ -21,19 +72,19 @@
 // }
 
 // BRUTE FORCE
-func nextGreaterElements(nums []int) []int {
-	length := len(nums)
-	ans := make([]int, 0)
-	for i := 0; i < length; i++ {
-		for j := i; j < (i + length); j++ {
-			if nums[j%length] > nums[i] {
-				ans = append(ans, nums[j%length])
-				break
-			}
-		}
-		if len(ans) == i {
-			ans = append(ans, -1)
-		}
-	}
-	return ans
-}
+// func nextGreaterElements(nums []int) []int {
+// 	length := len(nums)
+// 	ans := make([]int, 0)
+// 	for i := 0; i < length; i++ {
+// 		for j := i; j < (i + length); j++ {
+// 			if nums[j%length] > nums[i] {
+// 				ans = append(ans, nums[j%length])
+// 				break
+// 			}
+// 		}
+// 		if len(ans) == i {
+// 			ans = append(ans, -1)
+// 		}
+// 	}
+// 	return ans
+// }
