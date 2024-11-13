780200c210f9f632e633ebabe59e26b34badcf8b
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-11-13 14:46:14 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (7.42%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.go
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -1,4 +1,30 @@
+// Optimised soluton by precomputing lmax and rmax
 func trap(height []int) int {
+
+  n := len(height)
+  if n < 2 {return 0}
+  lmax,rmax := make([]int,n),make([]int,n)
+  lmax[0],rmax[n-1] = height[0],height[n-1]
+  res :=0
+
+  for i:=1;i<n;i++ {
+    lmax[i] = max(lmax[i-1],height[i])
+  }
+
+  for i:=n-2;i>0;i-- {
+    rmax[i] = max(rmax[i+1],height[i])
+  }
+
+  for k:=1 ;k<n-1;k++ {
+    res += (min(lmax[k],rmax[k]) - height[k])
+  }
+  
+  return res
+}
+
+
+// TC : O(N2) , SC : O(1)
+/*func trap(height []int) int {
     n := len(height)
     if n < 2 {return 0}
     result := 0
@@ -18,4 +44,4 @@ func trap(height []int) int {
         result += (min(leftMax,rightMax) - height[i])
     }
     return result
-}
+}*/
