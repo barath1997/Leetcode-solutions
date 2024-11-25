79d29b784af92fff0a1826655a768b7bed9064d0
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-11-25 09:05:59 +0530

    Time: 0 ms (100.00%) | Memory: 4.5 MB (9.17%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.go
+++ 206-reverse-linked-list/reverse-linked-list.go
@@ -7,7 +7,7 @@
  */
 
  // Optimised TC : O(N/2) , SC : O(1)
- func reverseList(head *ListNode) *ListNode {
+ /*func reverseList(head *ListNode) *ListNode {
 
     if head == nil {return head}
 
@@ -16,42 +16,49 @@
     var curr *ListNode = head
 
     for curr != nil {
-        next = curr.Next
-        curr.Next = prev
+        next = curr.Next // Store the Next Node before Changing curr 
+        curr.Next = prev // Actual Reversal
         
+        // Move prev and next by 1 step
         prev = curr
         curr = next
     }
 
     head = prev
     return head
- }
+ }*/
 
  // TC : O(2N) , SC : O(N)  stack based solution
-/*func reverseList(head *ListNode) *ListNode {
+func reverseList(head *ListNode) *ListNode {
 
 	if head == nil || head.Next == nil {return head}
  
-	temp := head
+	curr := head
 	
 	var st Stack
  
 	// inserting nodes into stack : O(n)
-	for temp != nil {
-	  st.Push(temp)
-	  temp = temp.Next
-	} 
- 
-	ll := &ListNode{Val: -1,Next:nil}
-    temp = ll
-	for !st.IsEmpty() {
-       
-       temp.Next = st.Pop()
-       temp = temp.Next
-	}
-	
-	temp.Next = nil
-	return ll.Next
+    for curr != nil {
+        st.Push(curr)
+        curr = curr.Next
+    }
+
+    // creating a new linkedList
+    res := &ListNode{Val:-1,Next:nil}
+    curr = res
+    
+    // till stack is not empty, pop elements and put inside a linkedlist
+    for !st.IsEmpty() {
+        curr.Next = st.Pop()
+        curr = curr.Next
+    }
+    
+    // curr is now at last node, now last node should be directing to nil
+    curr.Next = nil
+    
+    // since res has first element as -1 so we have to return res.Next, which has all the elements in reverse.
+    return res.Next
+
  }
  
  type Stack []*ListNode
@@ -79,4 +86,4 @@
 	 }
 	 return nil
  
- }*/
+ }
