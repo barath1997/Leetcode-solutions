43615e6b9817255e10ac87acfef68dce956f5ee5
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-12-09 09:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 5.1 MB (21.51%) - LeetSync

--- 837-most-common-word/most-common-word.go
+++ 837-most-common-word/most-common-word.go
@@ -1,32 +1,42 @@
 import "strings"
 
 func mostCommonWord(paragraph string, banned []string) string {
-
+    
+    // split the parapgrpah into words using multiple delimeters
     para := strings.FieldsFunc(paragraph,split)
 
     wordFreq := make(map[string]int)
 
     bannedList := make(map[string]bool,len(banned))
-
+    
+    // add banned words as lowercase into map , it is done so that search opearation takes O(1)
     for _,v := range banned {
         bannedList[strings.ToLower(string(v))] = true
     }
-
+    
+    // iterating over paragrapgh
     for _,v := range para {
+
+      // if word is in bannedlist or is a punctuation then skip it dont add it to wordFreq Map
       if bannedList[strings.ToLower(string(v))] ||  checkWordIsPunctuation(string(v)) {
          continue
       }
+
+      // add only good words to map and increase their frequencey
       wordFreq[strings.ToLower(v)]++
     }
     
     result , maxFreq := "",-1
+
+    // check for maxFreqency among the good words
     for word,freq := range wordFreq {
         if  freq > maxFreq {
             maxFreq = freq
             result = word
         }
     }
-
+    
+    // return the word which has maxFreqency among the good words
     return result
     
 }
@@ -37,18 +47,6 @@ func checkWordIsPunctuation(str string) bool {
     }
 
     return false
-}
-
-func checkBannedWord(word string, bannedList []string) bool {
-      
-      for _,v := range bannedList {
-        if strings.ToLower(v) == strings.ToLower(word) {
-            return true
-        }
-      }
-
-      return false
-
 }
 
 func split(str1 rune) bool{
