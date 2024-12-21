e4ef8b9f9b14ec7b5f7f1a574991516c72773be8
barath1997 <49673400+barath1997@users.noreply.github.com>
2024-12-21 10:41:30 +0530

    Time: 1 ms (82.15%) | Memory: 3.8 MB (9.90%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.go
+++ 1013-fibonacci-number/fibonacci-number.go
@@ -1,3 +1,20 @@
+// using DP
+func fib(n int) int {
+
+   if n ==0 {return 0} 
+   
+   res := make([]int,n+1)
+   if  n <= 2 {return 1}
+   res[1] = 1
+   res[2] = 1
+
+   for i:=3;i<=n;i++ {
+    res[i] = res[i-1] + res[i-2]
+   } 
+   
+   return res[n]
+}
+
 // using iterative approach
 /*func fib(n int) int {
 
@@ -17,11 +34,11 @@
 }*/
 
 // using recursive approach
-func fib(n int) int {
+/*func fib(n int) int {
    
     if n == 1 || n ==0 {
         return n
     }
     return fib(n-1) + fib(n-2)
 
-}
+}*/
